55b9536c165470a4699f2ddeeccfb3dfbaaabc77
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-02 11:49:21 +0600

    added /lib folder to autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ Bundler.require(*Rails.groups)
 
 module TrelloCore
   class Application < Rails::Application
+    config.autoload_paths << "#{config.root}/lib"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
