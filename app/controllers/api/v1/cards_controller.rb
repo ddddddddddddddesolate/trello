98bb02bc287da3076c6127e42cef44f58fcf1fb2
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-02 15:51:50 +0600

    implemented index method for card controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -3,6 +3,21 @@
 module Api
   module V1
     class CardsController < ApplicationController
+      def index
+        result = Cards::GetCardsService.call(current_user, column_params)
+
+        render json: result.cards
+      end
+
+      private
+
+      def current_user
+        params.require(:user_id)
+      end
+
+      def column_params
+        params.require(:column_id)
+      end
     end
   end
 end
---
+++ app/services/cards/get_cards_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Cards
+  class GetCardsService
+    include Callable
+
+    attr_reader :user_id, :column_id
+
+    def initialize(user_id, column_id)
+      @user_id = user_id
+      @column_id = column_id
+    end
+
+    def call
+      column = Column.find_by!(id: @column_id)
+      cards = column.cards
+
+      OpenStruct.new(cards: cards)
+    end
+  end
+end
