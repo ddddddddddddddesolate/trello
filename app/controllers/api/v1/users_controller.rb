e227d2ec0e4b53a034a66f8d7dce0a434ed0c747
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-05 16:33:19 +0600

    updated routing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,31 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_api_v1_user!, except: [:index]
+
+      def index
+        users = User.all
+        render json: users,
+               status: :ok
+      end
+
+      def show
+        result = Users::GetUserService.call(@current_api_v1_user, user_params)
+
+        if result.success
+          render json: result.user,
+                 status: :ok
+        else
+          render json: result.errors,
+                 status: :forbidden
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:id)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :comments
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'users'
 
-      resources :users do
+      resources :users, except: [:create] do
         resources :columns do
           resources :cards do
             resources :comments
