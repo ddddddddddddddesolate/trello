7af50c91f90282bde07fc5346b743332c6dcd3b4
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-05 17:55:05 +0600

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users'
-
-      resources :users, except: [:create] do
-        resources :columns do
-          resources :cards do
-            resources :comments
-          end
+      resources :users, except: [:create]
+      resources :columns do
+        resources :cards do
+          resources :comments
         end
       end
     end
