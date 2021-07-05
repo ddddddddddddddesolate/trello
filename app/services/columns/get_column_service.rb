28231ec44c50ca42fd25cf1a2c7411196fd1ee05
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-05 18:09:02 +0600

    updated GetColumnService

--- app/services/columns/get_column_service.rb
+++ app/services/columns/get_column_service.rb
@@ -4,18 +4,16 @@ module Columns
   class GetColumnService
     include Callable
 
-    attr_reader :user_id, :id
+    attr_reader :current_user, :id
 
-    def initialize(user_id, id)
-      @user_id = user_id
+    def initialize(current_user, id)
+      @current_user = current_user
       @id = id
     end
 
     def call
-      column = Column.find_by!(user_id: @user_id, id: @id)
-
-      # TODO: add user access checking
-
+      raise Exceptions::Unauthorized, 'Unauthorized' unless @current_user.present?
+      column = Column.find_by!(id: @id)
       OpenStruct.new(column: column)
     end
   end
