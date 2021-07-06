cc5c49f1f9e9e025eff92ee939db44ba2340c59f
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-06 10:59:56 +0600

    fixed missing semicolon in card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,7 +6,7 @@ class Card < ApplicationRecord
   has_many :comments, dependent: :delete_all
 
   validates :column, presence: true
-  validates :user, presence :true
+  validates :user, presence: :true
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
   validates :text, length: { maximum: 100 }, allow_blank: true
 end
