10ae5dc845430bc88a37061c1605b9b5c300e3df
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-06 11:44:58 +0600

    updated UpdateCardService

--- app/services/cards/update_card_service.rb
+++ app/services/cards/update_card_service.rb
@@ -4,20 +4,22 @@ module Cards
   class UpdateCardService
     include Callable
 
-    attr_reader :user_id, :column_id, :id, :card_params
+    attr_reader :current_user, :column_params, :id, :card_params
 
-    def initialize(user_id, column_id, id, card_params)
-      @user_id = user_id
-      @column_id = column_id
+    def initialize(current_user, column_params, id, card_params)
+      @current_user = current_user
+      @column_params = column_params
       @id = id
       @card_params = card_params
     end
 
     def call
-      column = Column.find_by!(id: @column_id, user_id: @user_id)
+      raise Exceptions::Unauthorized, 'Unauthorized' unless @current_user.present?
+      column = Column.find_by!(id: @column_params[:column_id])
       card = column.cards.find_by!(id: @id)
+      raise Exceptions::Forbidden, 'Forbidden' unless card.user == @current_user
 
-      OpenStruct.new(success: card.update?(@card_params), errors: card.errors, card: card)
+      OpenStruct.new(success: card.update(@card_params), errors: card.errors, card: card)
     end
   end
 end
